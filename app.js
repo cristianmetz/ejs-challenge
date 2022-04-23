fda8b18a86a2856d324e23824ae2ea22ac79574e
Cristian Metz <c.metz@outlook.com>
2022-04-23 13:17:21 -0400

    added foreach loop

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const res = require("express/lib/response");
 const req = require("express/lib/request");
-const { redirect } = require("express/lib/response");
+const {
+  redirect
+} = require("express/lib/response");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -24,7 +26,8 @@ app.get("/", (req, res) => {
   res.render("home", {
     data: {
       homeStartingContent: homeStartingContent,
-    posts: posts}
+      posts: posts
+    }
   });
 
 });
@@ -41,14 +44,14 @@ app.get("/contact", (req, res) => {
   });
 });
 
-app.get("/compose", (req,res) => {
+app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.post("/compose", (req,res) => {
+app.post("/compose", (req, res) => {
 
   const post = {
-    pTitle:req.body.postTitle,
+    pTitle: req.body.postTitle,
     pBody: req.body.postBody
   };
 
