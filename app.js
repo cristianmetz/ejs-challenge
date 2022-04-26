c22a110ab155adb73f230c73d2142a5987614ead
Cristian Metz <c.metz@outlook.com>
2022-04-26 19:32:24 -0400

    added post

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+// Load the full lodash build.
+var _ = require('lodash');
 const res = require("express/lib/response");
 const req = require("express/lib/request");
 const {
   redirect
 } = require("express/lib/response");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -68,13 +71,17 @@ app.post("/compose", (req, res) => {
 //route parameter
 app.get('/post/:postId', (req, res) => {
 
-  const requestedTitle = req.params.postId;
+  const requestedTitle = _.lowerCase(req.params.postId);
 
   posts.forEach(post => {
-    const storedTitle = post.pTitle;
+    const storedTitle = _.lowerCase(post.pTitle);
 
     if (requestedTitle === storedTitle) {
-      console.log("Match found!");
+      res.render("post", {
+        title: post.pTitle,
+        content: post.pBody
+
+      });
     }
   });
 
