488d8ebdac2fbb208b742c501a9e22c497123c70
Cristian Metz <c.metz@outlook.com>
2022-05-14 15:04:40 -0400

    completed challenge

--- app.js
+++ app.js
@@ -13,11 +13,9 @@ const {
 const dotenv = require("dotenv");
 dotenv.config();
 
-
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts = [];
 
 const app = express();
 
@@ -30,28 +28,24 @@ app.use(express.static("public"));
 
 const apiKey = process.env.MONGODB_ATLAST_PSW
 
-mongoose.connect(`mongodb+srv://admin-cristian:${apiKey}@cluster0.d44wg.mongodb.net/blogDB`);
+mongoose.connect(`mongodb+srv://admin-cristian:${apiKey}@cluster0.d44wg.mongodb.net/blogDB`, {
+  useNewUrlParser: true
+});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", (req, res) => {
 
-  res.render("home", {
-    data: {
+  Post.find({}, function (err, posts) {
+    res.render("home", {
       homeStartingContent: homeStartingContent,
       posts: posts
-    }
-  });
-
-});
-
-app.get("/about", (req, res) => {
-  res.render("about", {
-    aboutContent: aboutContent
-  });
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact", {
-    contactContent: contactContent
+    });
   });
 });
 
@@ -62,45 +56,63 @@ app.get("/compose", (req, res) => {
 
 app.post("/compose", (req, res) => {
 
-  const post = {
-    pTitle: req.body.postTitle,
-    pBody: req.body.postBody
-  };
-
-  posts.push(post);
-
-  res.redirect("/");
-});
-
-
-
-
-
-//route parameter
-app.get('/post/:postId', (req, res) => {
-
-  const requestedTitle = _.lowerCase(req.params.postId);
-
-  posts.forEach(post => {
-    const storedTitle = _.lowerCase(post.pTitle);
-
-    if (requestedTitle === storedTitle) {
-      res.render("post", {
-        title: post.pTitle,
-        content: post.pBody
-
-      });
-    }
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
   });
 
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
+app.post("/delete", function (req, res) {
+
+  const idDelete = req.body.button;
+
+  Post.findByIdAndRemove(idDelete, function (err) {
+    if (!err) {
+      console.log("successfully deleted");
+    }
+    res.redirect("/");
+  });
+
+
+});
+
+//route parameter
+app.get("/posts/:postId", function(req, res){
+
+  const requestedPostId = req.params.postId;
+  
+    Post.findOne({_id: requestedPostId}, function(err, post){
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    });
+  
+  });
+
+app.get("/about", function (req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
+});
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Server has started successfully.");
 });
